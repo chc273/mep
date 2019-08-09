61ab61e1c506e4fd985ac8ea6dee0bce876317b1
Chi Chen <chc273@eng.ucsd.edu>
2019-08-09 13:08:23 -0700

    update release files

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+import os
+
+DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+with open(os.path.join(DIR, "README.md"), "r") as f:
+    long_description = f.read()
 
 setup(
     name='mep',
@@ -7,12 +13,27 @@ setup(
     packages=find_packages(),
     author='Chi Chen',
     author_email="chc273@eng.ucsd.edu",
-    description='Find minimal energy path for atomistic systems',
-    long_description='Find minimal energy path for atomistic systems',
+    description='Minimal energy path tools for atomistic systems',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     download_url='https://github.com/chc273/mep',
-    install_requires=[
-        "numpy >= 1.9.0",
-        "scipy >= 0.14.0",
-        "scikit-learn >= 0.18.0",
+    keywords=["materials", "science", "nudged elastic band"],
+    license='BSD',
+    install_requires=['numpy', 'pymatgen', 'scipy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Software Development :: Libraries :: Python Modules"
     ],
+
+
 )
